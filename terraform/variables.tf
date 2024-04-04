24651aa3c49e15a88579fb8b8ce3a69bb81e7936
Baba Soori <bsoori@nuagique.ca>
2024-04-03 20:33:12 -0400

    Add tf vars

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "my_github_token" {
+  sensitive = true
+}
+variable "openai_api_key" {
+  sensitive = true
+}
+variable "assistant_id" {
+  sensitive = true
+}
+variable "authorized_repo" {
+  sensitive = true
+}
