1ac795387cb6d4004f53e5f36a6ffee816038174
Baba Soori <bsoori@nuagique.ca>
2024-04-05 13:52:39 -0400

    Fix Key policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -42,23 +42,6 @@ data "aws_iam_policy_document" "lambda_logs" {
 
 resource "aws_kms_key" "log_key" {
   description = "KMS key for cloudwatch log group"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "key-default-1",
-  "Statement": [
-    {
-      "Sid": "Enable IAM User Permissions",
-      "Effect": "Allow",
-      "Principal": {
-        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-      },
-      "Action": "kms:*",
-      "Resource": "*"
-    }
-  ]
-}
-POLICY
   enable_key_rotation = true
 }
 
