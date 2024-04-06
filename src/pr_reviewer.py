338156785ea64abb0dd917aedd68abda883a3438
Baba Soori <bsoori@nuagique.ca>
2024-04-06 15:24:43 -0400

    Add better logging (#2)

--- src/pr_reviewer.py
+++ src/pr_reviewer.py
@@ -1,8 +1,25 @@
+"""
+This module implements the PR Reviewer API using OpenAI
+
+The API takes a GitHub Pull Request URL as input and uses OpenAI to review the PR
+changes and provide feedback as a comment on the PR.
+
+The API is implemented as an AWS Lambda function and is triggered by an HTTP POST request
+from the API Gateway.
+
+The API expects the following environment variables to be set:
+- MY_GITHUB_TOKEN: GitHub Personal Access Token
+- OPENAI_API_KEY
+"""
+
 import os
-from github import Github
-from openai import OpenAI
 import time
 import logging
+
+from github import Github
+from openai import OpenAI
+
+ERROR_MESSAGE = "An unexpected error occurred while processing the request. Please try again later."
 
 logger = logging.getLogger()
 logger.setLevel("INFO")
@@ -17,7 +34,7 @@ def review_pull_request(repo_name, pr_number):
     """
     # Extract the secrets
 
-    print(f"Reviewing PR {pr_number} in repo {repo_name}")
+    logger.debug({"Reviewing PR": pr_number, "repo": repo_name})
 
     my_github_token = os.getenv('MY_GITHUB_TOKEN')
     openai_api_key = os.getenv('OPENAI_API_KEY')
@@ -46,14 +63,17 @@ def review_pull_request(repo_name, pr_number):
     prompt = (
         f"Review the following GitHub Pull Request changes:\n"
         f"{changes}\n"
-        f"Provide a brief description of the changes, make suggestions for improvement of the code and provide code"
-        f"snippet with your suggestions and for refactoring the code for the pull request. Always add a code snippet "
-        f"with a suggestion and one for refactoring. This will give the developer new ideas to improve their code\n "
-        f"Your feedback will be posted as a comment for the PR. Also when you add a suggestion add a corresponding "
-        f"code snippet to explain the improvement. Also give the HOW TO DO the changes or improvement you suggest\n"
-        f"This will provide the developer with useful insights for improving the code and making the pull request "
-        f"better\n. "
-        f"The suggestion and especially the code snippets are for giving tangible and actionable feedback. \n"
+        f"Provide a brief description of the changes, make suggestions for improvement of the code "
+        f"and provide code snippet with your suggestions and for refactoring the code for the pull "
+        f"request. Always add a code snippet with a suggestion and one for refactoring. This will "
+        f"give the developer new ideas to improve their code\n"
+        f"Your feedback will be posted as a comment for the PR. Also when you add a suggestion add "
+        f"a corresponding code snippet to explain the improvement. Also give the HOW TO DO the "
+        f"changes or improvement you suggest\n"
+        f"This will provide the developer with useful insights for improving the code and making "
+        f"the pull request better.\n"
+        f"The suggestion and especially the code snippets are for giving tangible and actionable "
+        f"feedback. \n"
         f"Thanks for you help.")
 
     run = client.beta.threads.create_and_run(
@@ -78,7 +98,7 @@ def review_pull_request(repo_name, pr_number):
         )
         logging.info({"Messages": messages})
     else:
-        logging.info({"Run Status": run.status})
+        logging.debug({"Run Status": run.status})
 
     # # Extract and post the OpenAI response as a PR comment
     logging.info({"OpenAI Response": messages.data[0].content[0].text.value})
@@ -90,27 +110,47 @@ def lambda_handler(event, context):
     Lambda handler for the PR Reviewer API
     :param event:
     :param context:
-    :return:
+    :return: response
     """
     logging.info({"event": event, "context": context})
 
-    logging.info({"queryStringParameters": event.get("queryStringParameters")})
+    logging.debug({"queryStringParameters": event.get("queryStringParameters")})
 
-    body = ("Hello Baba Demo. The OpenAI PR Reviewer API is still constructions. Check back in a few days. "
+    body = ("Hello Baba Demo.\n "
+            "The OpenAI PR Reviewer API is still constructions.\n"
+            "Check back in a few days.\n"
             "Thanks for your patience!")
 
+    status_code = 200
+
     query_string_parameters = event.get("queryStringParameters", {})
-    if query_string_parameters:
-        repo = query_string_parameters.get("repo")
-        if repo in os.getenv('AUTHORIZED_REPO').split(","):
-            logging.info({"repo": repo})
-            comment = review_pull_request(repo, int(event["queryStringParameters"]["pr"]))
-            body = comment.body
-        else:
-            body = "You are not unauthorized to use PR Reviewer on this repository !!!"
+
+    try:
+        if query_string_parameters:
+            repo = query_string_parameters.get("repo")
+            if repo in os.getenv('AUTHORIZED_REPO').split(","):
+                logging.debug({"repo": repo})
+                comment = review_pull_request(repo, int(event["queryStringParameters"]["pr"]))
+                body = comment.body
+            else:
+                logging.warning({"Unauthorized Repo": repo})
+                body = "You are not unauthorized to use PR Reviewer on this repository !!!"
+                status_code = 403
+    except KeyError as error:
+        logging.error({"KeyError": error})
+        body = ERROR_MESSAGE
+        status_code = 500
+    except SyntaxError as error:
+        logging.error({"SyntaxError": error})
+        body = ERROR_MESSAGE
+        status_code = 500
+    except ImportError as error:
+        logging.error({"ImportError": error})
+        body = ERROR_MESSAGE
+        status_code = 500
 
     res = {
-        "statusCode": 200,
+        "statusCode": status_code,
         "headers": {
             "Content-Type": "*/*"
         },
--- test/test_pr_reviewer.py
+++ test/test_pr_reviewer.py
@@ -1,13 +1,30 @@
+"""
+This module contains tests for the pr_reviewer module.
+"""
+
 import os
-import pytest
 from unittest.mock import patch, MagicMock
+import pytest
+
 from src import pr_reviewer
+
+ERROR_MESSAGE = 'An unexpected error occurred while processing the request. Please ' \
+    'try again later.'
+
 
 @patch('src.pr_reviewer.Github')
 @patch('src.pr_reviewer.OpenAI')
-def test_review_pull_request(mock_openai, mock_github):
+def test_review_pull_request_no_files(mock_openai, mock_github):
+    """
+    Test the review_pull_request function when the PR has no files
+    :param mock_openai:
+    :param mock_github:
+    :return:
+    """
     # Set up mock objects
     mock_github.return_value.get_repo.return_value.get_pull.return_value.get_files.return_value = []
+    mock_github.return_value.get_repo.return_value.get_pull.return_value.create_issue_comment.\
+        return_value = None
     mock_openai.return_value.beta.threads.create_and_run.return_value.status = 'completed'
     mock_openai.return_value.beta.threads.messages.list.return_value.data = [MagicMock()]
 
@@ -15,23 +32,60 @@ def test_review_pull_request(mock_openai, mock_github):
     result = pr_reviewer.review_pull_request('repo_name', 1)
 
     # Assert that the function behaves as expected
-    assert result is not None
+    assert result is None
+
 
 @patch('src.pr_reviewer.review_pull_request')
-def test_lambda_handler(mock_review_pull_request):
+def test_lambda_handler_unauthorized_repo(mock_review_pull_request):
+    """
+    Test the lambda_handler function when the repo is unauthorized
+    :param mock_review_pull_request:
+    :return:
+    """
     # Set up mock objects
     mock_review_pull_request.return_value.body = 'body'
 
     # Call the function with mock objects
     event = {
         "queryStringParameters": {
-            "repo": "repo",
+            "repo": "unauthorized_repo",
             "pr": "1"
         }
     }
-    os.environ['AUTHORIZED_REPO'] = 'repo'
+    os.environ['AUTHORIZED_REPO'] = 'authorized_repo'
     result = pr_reviewer.lambda_handler(event, None)
 
     # Assert that the function behaves as expected
-    assert result['statusCode'] == 200
-    assert result['body'] == 'body'
+    assert result['statusCode'] == 403
+    assert result['body'] == 'You are not unauthorized to use PR Reviewer on this repository !!!'
+
+
+@pytest.mark.parametrize("error, expected_status_code, expected_body", [
+    (KeyError('error'), 500, ERROR_MESSAGE),
+    (SyntaxError('error'), 500, ERROR_MESSAGE),
+    (ImportError('error'), 500, ERROR_MESSAGE)
+])
+@patch('src.pr_reviewer.review_pull_request')
+def test_lambda_handler_error_occurred(mock_review_pull_request, error, expected_status_code,
+                                       expected_body):
+    """
+    Test the lambda_handler function when an error occurred
+    :param mock_review_pull_request:
+    :return:
+    """
+    # Set up mock objects
+    mock_review_pull_request.side_effect = error
+
+    # Call the function with mock objects
+    event = {
+        "queryStringParameters": {
+            "repo": "authorized_repo",
+            "pr": "1"
+        }
+    }
+    os.environ['AUTHORIZED_REPO'] = 'authorized_repo'
+    result = pr_reviewer.lambda_handler(event, None)
+
+    # Assert that the function behaves as expected
+    assert result['statusCode'] == expected_status_code
+    assert result['body'] == expected_body
